0b319851730aa0354cdd084aefdb22b75aa404ee
Joseph Fay <joegfay@gmail.com>
2024-12-07 12:30:44 -0600

    Add drawBar function to meStorage module for visual output

--- meStorage.lua
+++ meStorage.lua
@@ -1,3 +1,10 @@
 -- Written by Balsac
 
 local bridge = peripheral.find('meBridge')
+
+
+function drawBar()
+    term.setCursorPos(1, 1)
+    -- drawBox(startX, startY, endX, endY [, colour])
+    paintutils.drawBox(3, 3, 38, 5, colors.gray)
+end
