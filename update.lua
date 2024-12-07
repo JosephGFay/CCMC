876ff1650228beada173088695409cfc5b26ef83
Joseph Fay <joegfay@gmail.com>
2024-12-06 21:34:09 -0600

    Refactor update script to support multiple URLs and improve error handling

--- update.lua
+++ update.lua
@@ -1,10 +1,13 @@
-local url = 'https://raw.githubusercontent.com/JosephGFay/CCMC/refs/heads/main/main.lua'
-local filename = main.lua
+local urls = {
+    main = 'https://raw.githubusercontent.com/JosephGFay/CCMC/refs/heads/main/main.lua',
+}
 
-local success, err = pcall(function()
-    shell.run('rom/programs/http/wget.lua', url, filename)
-end)
-
-if success then
-    print('Update Successful')
+for _, url in pairs(urls) do
+    local success, err = pcall(function()
+        shell.run('rom/programs/http/wget.lua', url, _ .. '.lua')
+    end)
+    
+    if success then
+        print('Update Successful')
+    end
 end
