f73acb953fa83d7930c206232f9c15fc21c6646a
Joseph Fay <joegfay@gmail.com>
2024-12-06 21:53:29 -0600

    Enhance menu display with indexed options and add debug cursor position output

--- main.lua
+++ main.lua
@@ -2,8 +2,8 @@
 
 
 local menu_options = {
-    'Map',
-    'Inventory',
+    '[0] - Map',
+    '[1] - Inventory',
 }
 
 local function draw_menu()
@@ -14,7 +14,11 @@ local function draw_menu()
     for i, option in ipairs(menu_options) do
         print(i .. '. ' .. option)
     end
+    print(' ')
+    print('[DEBUG]: Cursor Position: ' .. term.getCursorPos())
 end
+
+
 
 function main()
     draw_menu()
@@ -23,5 +27,15 @@ function main()
         if key == keys.q then
             break
         end
+
+        
+        if key == keys[0] then
+            print('Map')
+        else if key == keys[1] then
+            print('Inventory')
+        end
+        end
     end
 end
+
+main()
