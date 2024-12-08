26645fcd8ea9f95e5942671e73594c873d456b17
Joseph Fay <joegfay@gmail.com>
2024-12-08 00:33:52 -0600

    Add factory control interface with menu and status display

---
+++ factory.lua
@@ -0,0 +1,82 @@
+term.setTextColor(colors.lime)
+local monitor = peripheral.find('monitor')
+monitor.setTextScale(1)
+monitor.write([[
+  ___   _   _    ___   _   ___        _ 
+ | _ ) /_\ | |  / __| /_\ / __| __ __/ |
+ | _ \/ _ \| |__\__ \/ _ \ (__  \ V /| |
+ |___/_/ \_\____|___/_/ \_\___|  \_(_)_|
+                                                                                       
+    ]])
+
+local menu_options = {
+    '[0] - Factory: On',
+    '[1] - Factory: Off',
+}
+
+local status
+
+local function factoryOn()
+    redstone.setOutput('left', false)
+    status = true
+end
+
+local function factoryOff()
+    redstone.setOutput('left', true)
+    status = false
+end
+
+local function getFactoryStatus()
+    return 'Factory Status: ' .. ((status) and 'Online' or 'Offline')
+end
+
+
+local function draw_menu()
+    term.clear()
+    term.setCursorPos(1, 1)
+    print([[
+  ___   _   _    ___   _   ___        _ 
+ | _ ) /_\ | |  / __| /_\ / __| __ __/ |
+ | _ \/ _ \| |__\__ \/ _ \ (__  \ V /| |
+ |___/_/ \_\____|___/_/ \_\___|  \_(_)_|
+                                                                                       
+    ]])
+    print(' ')
+    print('Menu')
+    print('----')
+    for i, option in ipairs(menu_options) do
+        print(option)
+    end
+    print(' ')
+    print(getFactoryStatus())
+    print('Press Q to exit..')
+end
+
+
+
+function main()
+   
+    while true do
+        draw_menu()
+        local event, key = os.pullEvent('key')
+        if key == keys.q then
+            break
+        end
+
+        -- Menu Options
+        if key == keys.tab then
+            draw_menu()
+        end
+
+        if key == keys.zero then
+            factoryOn()
+        end
+    
+        if key == keys.one then
+            factoryOff()
+        end
+    
+    end
+end
+
+main()
